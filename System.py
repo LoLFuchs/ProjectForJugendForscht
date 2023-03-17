abf620828cd9ff32c6325d5a139998bce5bbbd1e
LoLFuchs <119937576+LoLFuchs@users.noreply.github.com>
2023-03-17 21:36:07 +0100

    SQLite into
    added: 
    -> SQLite for Datebase 
    -> Points for students + function for points add(later for rewards) 
    -> students will get a table for points 
    
    Deleted: 
    -> Test on the footer 
    -> bugs

--- System.py
+++ System.py
@@ -1,23 +1,50 @@
-# Dies dient zur veranschaulichung des Systems welches am Ende den Scanner darstellen wird um Hausaufgaben Hoch bzw. zu lesen
-
+import sqlite3
+import time 
 import Datenbank
 
+# Verbindung zur SQLite-Datenbank herstellen
+conn = sqlite3.connect('Datenbank.db')
 
-# KS = Klasse Schüler 
-# KL = Klasse Lehrer
+# Cursor-Objekt erstellen
+c = conn.cursor()
 
+# Tabelle 'schueler' erstellen, falls sie noch nicht existiert
+c.execute('''CREATE TABLE IF NOT EXISTS schueler
+             (name text PRIMARY KEY, punkte integer)''')
 
-
-def Trigger(Rolle, KL, value):
-
-    if Rolle == "Lehrer":
-        class_list = "Datenbank." + KL
-        exec(f"{class_list}.append('{value}')")
-        print(Datenbank.K8A)
-
-    elif Rolle == "Schueler": 
-          print("Deine Hausaufgaben sind:")
+def add_schueler(name): # add schüeler if name != Datenbank
+    c.execute("SELECT * FROM schueler WHERE name=?", (name,))
+    result = c.fetchone()
+    if result is None:
+        c.execute("INSERT INTO schueler VALUES (?,?)", (name, 0))
+        conn.commit()
+        print("Herzlich Willkommen im " + Datenbank.NameSchule)
+        time.sleep(4)
     
-#Trigger(Input, "Hausaufgaben") # Triggern der Founktion // Entweder auf der NFC Card speichern / oder im System.py ausfühern
+def add_punkte(name, punkte): # add Punkte zu dem Schueler / in System.py
+    c.execute("UPDATE schueler SET punkte = punkte + ? WHERE name=?", (punkte, name))
+    conn.commit()
+    print("added points to " + name)
+    
+def get_punkte(name): #return Punkte des Schueler / later: click on button to show on LCD in System.py
+    c.execute("SELECT punkte FROM schueler WHERE name=?", (name,))
+    result = c.fetchone()
+    return result[0] if result else 0
 
-#Idee: Punkte System ( Erledigte Hausaufgaben) / Hausaufgaben + Kalender
+def Trigger(role, klasse, value): # Teacher or Student 
+    class_list = getattr(Datenbank, klasse, None)
+
+    if class_list is None:
+        print(f"Klasse {klasse} nicht gefunden!")
+        return
+
+    if role == "Lehrer": #if role is lehrer do: 
+        class_list.append(value)
+        print(f"{value} zu Klasse {klasse} hinzugefügt.")
+    elif role == "Schueler":
+        if len(class_list) > 0:
+            print(f"Deine Hausaufgaben für {klasse} sind: {', '.join(class_list)}")
+        else:
+            print(f"Keine Hausaufgaben für {klasse} gefunden.")
+# Verbindung zur Datenbank schließen
+conn.close()
