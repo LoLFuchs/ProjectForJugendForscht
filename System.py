06d735529f7d0e3fcab43f6b54a5b9a44876d784
LoLFuchs <119937576+LoLFuchs@users.noreply.github.com>
2023-03-19 20:07:42 +0100

    Working Database
    added: 
    
    -> working Database for students and teachers
    -> auto create table for the classes 
    -> students now will get orderd by the class to find them simpler 
    -> Points now dont display "none" anymore they will start with 0 
    
    deleted: 
    
    -> minor bugs

--- System.py
+++ System.py
@@ -5,46 +5,74 @@ import Datenbank
 # Verbindung zur SQLite-Datenbank herstellen
 conn = sqlite3.connect('Datenbank.db')
 
-# Cursor-Objekt erstellen
-c = conn.cursor()
-
 # Tabelle 'schueler' erstellen, falls sie noch nicht existiert
+c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS schueler
-             (name text PRIMARY KEY, punkte integer)''')
+             (name text PRIMARY KEY, punkte integer, klasse text)''')
+conn.commit()
 
-def add_schueler(name): # add schüeler if name != Datenbank
+def add_schueler(name, klasse): # neuen Schüler hinzufügen
+    conn = sqlite3.connect('Datenbank.db')
+    c = conn.cursor()
     c.execute("SELECT * FROM schueler WHERE name=?", (name,))
     result = c.fetchone()
     if result is None:
-        c.execute("INSERT INTO schueler VALUES (?,?)", (name, 0))
+        c.execute("INSERT INTO schueler (name, punkte, klasse) VALUES (?, 0, ?)", (name, klasse))
         conn.commit()
         print("Herzlich Willkommen im " + Datenbank.NameSchule)
         time.sleep(4)
-    
-def add_punkte(name, punkte): # add Punkte zu dem Schueler / in System.py
+    conn.close()
+
+
+def add_punkte(name, punkte): # add Punkte zu dem Schueler / Hausaufgaben fertig
+    conn = sqlite3.connect('Datenbank.db')
+    c = conn.cursor()
     c.execute("UPDATE schueler SET punkte = punkte + ? WHERE name=?", (punkte, name))
     conn.commit()
     print("added points to " + name)
+    conn.close()
     
-def get_punkte(name): #return Punkte des Schueler / later: click on button to show on LCD in System.py
+def get_punkte(name): #return Punkte des Schueler / later: left click on button to show on LCD in System.py
+    conn = sqlite3.connect('Datenbank.db')
+    c = conn.cursor()
     c.execute("SELECT punkte FROM schueler WHERE name=?", (name,))
     result = c.fetchone()
+    conn.close()
     return result[0] if result else 0
 
-def Trigger(role, klasse, value): # Teacher or Student 
-    class_list = getattr(Datenbank, klasse, None)
+##########################################################################################################################
+# Teacher or Student 
 
-    if class_list is None:
-        print(f"Klasse {klasse} nicht gefunden!")
-        return
+def Trigger(role, klasse, value): 
+    # Verbindung zur SQLite-Datenbank herstellen
+    with sqlite3.connect('Datenbank.db') as conn:
+        c = conn.cursor()
 
-    if role == "Lehrer": #if role is lehrer do: 
-        class_list.append(value)
-        print(f"{value} zu Klasse {klasse} hinzugefügt.")
-    elif role == "Schueler":
-        if len(class_list) > 0:
-            print(f"Deine Hausaufgaben für {klasse} sind: {', '.join(class_list)}")
-        else:
-            print(f"Keine Hausaufgaben für {klasse} gefunden.")
-# Verbindung zur Datenbank schließen
-conn.close()
+        # Platzhalter-Zeichen für den Tabellennamen im SQL-Befehl
+        query = '''CREATE TABLE IF NOT EXISTS {} (name text PRIMARY KEY, punkte integer)'''.format(klasse)
+        c.execute(query)
+
+
+
+        if role == "Lehrer":
+            query = '''INSERT INTO {} VALUES (?, ?)'''.format(klasse)
+            c.execute(query, (value, 1))
+
+            print(f"{value} zu Klasse {klasse} hinzugefügt.")
+       
+
+
+        elif role == "Schueler":
+            query = '''SELECT name FROM {}'''.format(klasse)
+            c.execute(query)
+            results = c.fetchall()
+
+            if len(results) > 0:
+                 class_list = [result[0] for result in results]
+                 print(f"Die Hausaufgaben für {klasse} sind: {', '.join(class_list)}")
+            else:
+                 print(f"Keine Hausaufgaben für {klasse} gefunden.")
+        conn.commit()
+        conn.close
+
+
