abbffb07f9b22802650a5f36168c95360d88bcf4
LoLFuchs <119937576+LoLFuchs@users.noreply.github.com>
2023-04-04 13:47:30 +0200

    V. 0.1.13
    Made a mistake while uploading the file 
    this is the new file

--- System.py
+++ System.py
@@ -2,18 +2,11 @@ import sqlite3
 import time 
 import Datenbank
 
-# Verbindung zur SQLite-Datenbank herstellen
-conn = sqlite3.connect('Datenbank.db')
+# ALLE print() Commands WERDEN SPÄTER DURCH DEN COMMAND FÜR DEN LCD DISPLAY ERSTEZT     
 
-# Tabelle 'schueler' erstellen, falls sie noch nicht existiert
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS schueler
-             (name timport sqlite3
-import time 
-import Datenbank
-
-
-menu = 0 # menu = 1 (Main Menu)
+#        Name    role     klasse  value
+Save =  ["None", "None", "None", "None"]
+menu = 1 # menu = 1 (Main Menu)
 
 # Verbindung zur SQLite-Datenbank herstellen
 conn = sqlite3.connect('Datenbank.db')
@@ -58,10 +51,10 @@ def get_punkte(name): #return Punkte des Schueler / later: left click on button 
 
 def Trigger(name, role, klasse, value): 
     #Save the parameter for later
-    Sname = name
-    Srole = role 
-    Sklasse = klasse 
-    Svalue = value 
+    Save[0] = name
+    Save[1] = role 
+    Save[2] = klasse 
+    Save[3] = value
 
     # Verbindung zur SQLite-Datenbank herstellen
     with sqlite3.connect('Datenbank.db') as conn:
@@ -105,90 +98,26 @@ def Trigger(name, role, klasse, value):
     conn.commit()
     conn.close
 
-def LeftClick(menu = menu):
-    if Sname != "a":
+def LeftClick(menu = menu): # menu nach links
+    if Save[0] != "None":
         if menu > -1:  
-            menu = menu - 1
-            #Trigger(Sname,Srole,Sklasse,Svalue)
-        
+            menu -= 1
+            Trigger(Save[0], Save[1], Save[2], Save[3])
             print("Debug: " + str(menu))
         else: 
             print("Lege erst eine Karte ein")
 
-def RightClick(menu = menu):
-    if Sname != None:
+def RightClick(menu = menu): # Menu nach rechts
+    if Save[0] != "None":
         if menu < 3:  
-             menu = menu + 1
-        
-        print("Debug: " + str(menu))
-    else: 
-        print("Lege erst eine Karte ein")
+           menu += 1
+           print("Debug: " + str(menu))
+           Trigger(Save[0], Save[1], Save[2], Save[3])
+        else: 
+            print("Lege erst eine Karte ein")
 
-ext PRIMARY KEY, punkte integer, klasse text)''')
-conn.commit()
-
-def add_schueler(name, klasse): # neuen Schüler hinzufügen
-    conn = sqlite3.connect('Datenbank.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM schueler WHERE name=?", (name,))
-    result = c.fetchone()
-    if result is None:
-        c.execute("INSERT INTO schueler (name, punkte, klasse) VALUES (?, 0, ?)", (name, klasse))
-        conn.commit()
-        print("Herzlich Willkommen im " + Datenbank.NameSchule)
-        time.sleep(4)
-    conn.close()
-
-
-def add_punkte(name, punkte): # add Punkte zu dem Schueler / Hausaufgaben fertig
-    conn = sqlite3.connect('Datenbank.db')
-    c = conn.cursor()
-    c.execute("UPDATE schueler SET punkte = punkte + ? WHERE name=?", (punkte, name))
-    conn.commit()
-    print("added points to " + name)
-    conn.close()
-    
-def get_punkte(name): #return Punkte des Schueler / later: left click on button to show on LCD in System.py
-    conn = sqlite3.connect('Datenbank.db')
-    c = conn.cursor()
-    c.execute("SELECT punkte FROM schueler WHERE name=?", (name,))
-    result = c.fetchone()
-    conn.close()
-    return result[0] if result else 0
-
-##########################################################################################################################
-# Teacher or Student 
-
-def Trigger(role, klasse, value): 
-    # Verbindung zur SQLite-Datenbank herstellen
-    with sqlite3.connect('Datenbank.db') as conn:
-        c = conn.cursor()
-
-        # Platzhalter-Zeichen für den Tabellennamen im SQL-Befehl
-        query = '''CREATE TABLE IF NOT EXISTS {} (name text PRIMARY KEY, punkte integer)'''.format(klasse)
-        c.execute(query)
-
-
-
-        if role == "Lehrer":
-            query = '''INSERT INTO {} VALUES (?, ?)'''.format(klasse)
-            c.execute(query, (value, 1))
-
-            print(f"{value} zu Klasse {klasse} hinzugefügt.")
-       
-
-
-        elif role == "Schueler":
-            query = '''SELECT name FROM {}'''.format(klasse)
-            c.execute(query)
-            results = c.fetchall()
-
-            if len(results) > 0:
-                 class_list = [result[0] for result in results]
-                 print(f"Die Hausaufgaben für {klasse} sind: {', '.join(class_list)}")
-            else:
-                 print(f"Keine Hausaufgaben für {klasse} gefunden.")
-        conn.commit()
-        conn.close
-
-
+def Logout(): #Ausloggen
+    Save[0] = "None"
+    Save[1] = "None"
+    Save[2] = "None"
+    Save[3] = "None"
